97f69e136adcbf535290ee02754354d6411990e5
Scott Burns <scott@lentigo.net>
2014-07-07 22:03:46 -0400

    Add remaining dotfile symlinks to bootstrap.sh script

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,7 +3,10 @@ cd "$(dirname "${BASH_SOURCE}")"
 # git pull origin master
 function doIt() {
   # symlink managed .dotfiles to the home directory
-  for DOTFILE in .vimrc .gvimrc .vim .bash_aliases; do
+  for DOTFILE in .{vim,gvim}rc .vim .dircolors; do
+    ln -sTi $(pwd)/${DOTFILE} ~/${DOTFILE}
+  done
+  for DOTFILE in .bash_{aliases,exports,extra,profile,prompt} .bashrc; do
     ln -sTi $(pwd)/${DOTFILE} ~/${DOTFILE}
   done
 ##  rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" \
@@ -11,7 +14,7 @@ function doIt() {
     ##--exclude ".gitignore" \
     ##--exclude ".gitmodules" --exclude "README.md" \
     ##--exclude "LICENSE-MIT.txt" -av --no-perms . ~
-##  source ~/.bash_profile
+  source ~/.bash_profile
 }
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
   doIt
