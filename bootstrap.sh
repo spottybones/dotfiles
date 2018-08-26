81615f56d5ca8bcd3e728fb6cfe1a8762ff31df2
Scott Burns <sburns@centercitypcs.org>
2018-08-26 11:00:44 -0400

    Copy Files instead of Symlinking
    Move configuration files to subdirectory named HOME and then use rsync
    to copy them over orignals in $HOME. This makes for a much simpler
    bootstrap.sh process.

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,25 +2,10 @@
 cd "$(dirname "${BASH_SOURCE}")"
 # git pull origin ccpcs000536
 function doIt() {
-  # symlink managed .dotfiles to the home directory
-  for DOTFILE in .{vim,gvim}rc .vim .dircolors .tmux.conf .gitconfig; do
-    [ -f "$(pwd)/$DOTFILE" ] && ln -sTi $(pwd)/${DOTFILE} ~/${DOTFILE}
-  done
-
-  # copy bash configuration
-  for DOTFILE in .bash{rc,_profile,_logout}; do
-    [ -f ~/${DOTFILE} ] && rm ~/${DOTFILE}
-    cp ${DOTFILE} ~/${DOTFILE}
-  done
-  rsync -av ./.config ~/
-
-##  rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" \
-    ##--exclude ".vim" --exclude ".vimrc" --exclude ".gvimrc" \
-    ##--exclude ".gitignore" \
-    ##--exclude ".gitmodules" --exclude "README.md" \
-    ##--exclude "LICENSE-MIT.txt" -av --no-perms . ~
-  # source ~/.bash_profile
+  # rsync contents of HOME to $HOME
+  rsync -av ./HOME/ ~/
 }
+
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
   doIt
 else
