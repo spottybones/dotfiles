8400d17f73b79df226378018dc0aff4e4e5d9d36
Scott Burns <sburns@centercitypcs.org>
2019-02-22 10:37:59 -0700

    Fix Issues Idenfied by "shellcheck"

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-cd "$(dirname "${BASH_SOURCE}")"
+cd "$(dirname "${BASH_SOURCE[0]}")" || (echo "Unable to switch to script directory" && exit 1)
 
 export XDG_DATA_HOME="${HOME}/.local/share"
 export XDG_CONFIG_HOME="${HOME}/.config"
@@ -10,17 +10,17 @@ function doIt() {
   # rsync contents of HOME to $HOME
   rsync -av ./HOME/ ~/
   # ensure VIM temp directories exist
-  mkdir -p ${XDG_DATA_HOME}/vim/{backups,swaps,undo}
+  mkdir -p "${XDG_DATA_HOME}"/vim/{backups,swaps,undo}
   # ensure IPython/Jupyter temp directories exist
-  mkdir -p ${XDG_DATA_HOME}/{ipython,jupyter}
+  mkdir -p "${XDG_DATA_HOME}"/{ipython,jupyter}
   # install xterm-256color-italic TERM entry
   tic ./external_configs/xterm-256color-italic.terminfo
 }
 
-if [ "$1" == "--force" -o "$1" == "-f" ]; then
+if [ "$1" == "--force" ] || [ "$1" == "-f" ]; then
   doIt
 else
-  read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
+  read -rp "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
   echo
   if [[ $REPLY =~ ^[Yy]$ ]]; then
     doIt
