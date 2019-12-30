f6986f792d7894309621cce585b13013da11f1ea
Scott Burns <sburns@centercitypcs.org>
2019-12-30 15:17:16 -0500

    Use GNU *Utils from Homebrew
    Set PATH to favor GNU utilities provided by Homebrew over their MacOS
    equivalents. We use a oh-my-zsh custom configuration instead of the
    gnu-utils plugin as we want the GNU verions called by scripts as well.

---
+++ HOME/.config/zsh/gnu_utils.zsh
@@ -0,0 +1,13 @@
+# vim:set ft=sh:
+
+# favor GNU utilities provided by homebrew over MacOS versions
+gnu_pkgs=(coreutils findutils gnu-sed)
+
+for gnu_pkg in ${gnu_pkgs}; do
+  if [ -d /usr/local/opt/${gnu_pkg}/libexec/gnubin ]; then
+    export PATH="/usr/local/opt/${gnu_pkg}/libexec/gnubin:$PATH"
+    export MANPATH="/usr/local/opt/${gnu_pkg}/libexec/gnuman:$MANPATH"
+  fi
+done
+
+unset gnu_pkgs
