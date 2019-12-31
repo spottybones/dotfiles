734fed7e9b9844a8d9c3e1f0b82d2400c49349a3
Scott Burns <sburns@centercitypcs.org>
2019-12-31 11:47:00 -0500

    Update Location of Google Cloud SDK
    Point to the Homebrew Cask installation.

--- HOME/.config/zsh/google-cloud-sdk.zsh
+++ HOME/.config/zsh/google-cloud-sdk.zsh
@@ -1,2 +1,2 @@
 # The next line updates PATH for the Google Cloud SDK.
-export CLOUDSDK_HOME="${HOME}/Applications/google-cloud-sdk"
+export CLOUDSDK_HOME="/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk"
