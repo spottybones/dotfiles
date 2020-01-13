9cce815e579a77ce12a31255ceb45461d9127a92
Scott Burns <sburns@centercitypcs.org>
2020-01-13 15:47:28 -0500

    Use Homebrew Grep if Installed

--- HOME/.config/zsh/gnu_utils.zsh
+++ HOME/.config/zsh/gnu_utils.zsh
@@ -1,7 +1,7 @@
 # vim:set ft=sh:
 
 # favor GNU utilities provided by homebrew over MacOS versions
-gnu_pkgs=(coreutils findutils gnu-sed)
+gnu_pkgs=(coreutils findutils gnu-sed grep)
 
 for gnu_pkg in ${gnu_pkgs}; do
   if [ -d /usr/local/opt/${gnu_pkg}/libexec/gnubin ]; then
