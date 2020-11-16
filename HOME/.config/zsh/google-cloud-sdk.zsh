5f53817faaa005e639a293919fd0545644728cf9
Scott Burns <sburns@centercitypcs.org>
2020-11-16 11:09:43 -0500

    Speficy Python 3.8.5 for Google Cloud SDK

--- HOME/.config/zsh/google-cloud-sdk.zsh
+++ HOME/.config/zsh/google-cloud-sdk.zsh
@@ -1,2 +1,3 @@
 # The next line updates PATH for the Google Cloud SDK.
 export CLOUDSDK_HOME="/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk"
+export CLOUDSDK_PYTHON="$(pyenv prefix 3.8.5)/bin/python"
