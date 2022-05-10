396762fab323eeda8173cf5629c8d811d985b4cb
Scott Burns <sburns@centercitypcs.org>
2022-05-10 15:00:24 -0400

    Nvim: Set Default Options for Python Files

--- HOME/nvim/.config/nvim/after/ftplugin/py.lua
+++ HOME/nvim/.config/nvim/after/ftplugin/py.lua
@@ -1,5 +1,6 @@
-vim.bo.shiftwidth = 2
-vim.bo.tabstop = 2
-vim.bo.softtabstop = 2
-vim.bo.textwidth = 120
+-- Python Defaults
+vim.bo.shiftwidth = 4
+vim.bo.tabstop = 4
+vim.bo.softtabstop = 4
+vim.bo.textwidth = 80
 vim.bo.expandtab = true
