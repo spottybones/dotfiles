8658d3f0021dd66ad739877ac60ceb4b461e09d6
Scott Burns <scott@lentigo.net>
2023-03-02 15:14:07 -0500

    Lvim: Enable Additional Formatters for "python" Files
    Added "black" and "isort" for Python code.

--- HOME/lvim/.config/lvim/config.lua
+++ HOME/lvim/.config/lvim/config.lua
@@ -131,10 +131,10 @@ lvim.builtin.treesitter.highlight.enable = true
 -- end
 
 -- -- set a formatter, this will override the language server formatting capabilities (if it exists)
--- local formatters = require "lvim.lsp.null-ls.formatters"
--- formatters.setup {
---   { command = "black", filetypes = { "python" } },
---   { command = "isort", filetypes = { "python" } },
+local formatters = require "lvim.lsp.null-ls.formatters"
+formatters.setup {
+  { command = "black", filetypes = { "python" } },
+  { command = "isort", filetypes = { "python" } },
 --   {
 --     -- each formatter accepts a list of options identical to https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTINS.md#Configuration
 --     command = "prettier",
@@ -144,7 +144,7 @@ lvim.builtin.treesitter.highlight.enable = true
 --     ---@usage specify which filetypes to enable. By default a providers will attach to all the filetypes it supports.
 --     filetypes = { "typescript", "typescriptreact" },
 --   },
--- }
+}
 
 -- -- set additional linters
 -- local linters = require "lvim.lsp.null-ls.linters"
