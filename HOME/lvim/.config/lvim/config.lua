6fae5e9465a3cdb93c6e0f77fbb1989bbe3e5fce
Scott Burns <scott@lentigo.net>
2023-03-25 10:29:35 -0400

    Map Keys for Switching Buffers in LVIM

--- HOME/lvim/.config/lvim/config.lua
+++ HOME/lvim/.config/lvim/config.lua
@@ -21,6 +21,8 @@ lvim.leader = "space"
 lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
 lvim.keys.insert_mode["jk"] = "<esc>"
 lvim.keys.normal_mode["\\"] = ":nohl<cr>"
+lvim.keys.normal_mode["<C-h>"] = ":bp<cr>"
+lvim.keys.normal_mode["<C-l>"] = ":bn<cr>"
 -- lvim.keys.normal_mode["<S-l>"] = ":BufferLineCycleNext<CR>"
 -- lvim.keys.normal_mode["<S-h>"] = ":BufferLineCyclePrev<CR>"
 -- unmap a default keymapping
