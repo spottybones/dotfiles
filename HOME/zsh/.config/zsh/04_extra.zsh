cfec065ce7bd54401c40a56ce1311146c852f04d
Scott Burns <scott@lentigo.net>
2024-08-28 14:14:55 +0000

    Remove unused functions
    These don't work on modern Linux without 'ifconfig' and I don't use them
    anyway. I don't use 'vault' so there's no need to keep those
    completions.

--- HOME/zsh/.config/zsh/04_extra.zsh
+++ HOME/zsh/.config/zsh/04_extra.zsh
@@ -1,14 +1,3 @@
 # vim:set ft=sh:
 
 set -o vi
-
-# determine local IP addresses
-function localip() {
-    for IF in en{0,1,2,3,4}; do
-        local IP=$(ipconfig getifaddr ${IF})
-        if ! test -z "${IP}"
-        then
-            echo $IF: $IP
-        fi
-    done
-}
